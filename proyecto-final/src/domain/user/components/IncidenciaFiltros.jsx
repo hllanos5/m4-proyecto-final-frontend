bc40f19af22d5124dbccae5967d01b3e64617c33
Hans Llanos <hans.llanos@gmail.com>
2024-10-03 18:04:36 -0500

    filtro de incidencias

--- proyecto-final/src/domain/user/components/IncidenciaFiltros.jsx
+++ proyecto-final/src/domain/user/components/IncidenciaFiltros.jsx
@@ -7,7 +7,7 @@ import { Dropdown } from 'primereact/dropdown';
 import { Button } from 'primereact/button';
 import { Accordion, AccordionTab } from 'primereact/accordion';
 
-export default function IncidenciaFiltros() {
+export default function IncidenciaFiltros({setFiltros}) {
 
     const [dates, setDates] = useState(null);
     const [selectedEstado, setSelectedEstado] = useState(null);
@@ -29,37 +29,80 @@ export default function IncidenciaFiltros() {
         clear: 'Limpiar'
     });
 
+    const handleSetFiltros = async (e)=> {
+
+        e.preventDefault();
+
+        let estado = "";
+
+        switch (e.target.estado.value) {
+            case "0":
+                estado = "Abierta"
+                break;
+            case "1":
+                estado = "En Progreso"
+                break;
+            case "2":
+                estado = "Cerrada"
+                break;
+        }
+        const filtros = {} ;
+
+        if((e.target.id.value).length> 0){
+            filtros.id = parseInt(e.target.id.value)
+        }
+        if((e.target.nombres.value).length> 0){
+            filtros.nombres = e.target.nombres.value
+        }
+        if((e.target.titulo.value).length> 0){
+            filtros.titulo = e.target.titulo.value
+        }
+        if((e.target.fecha.value).length> 0){
+            const fechaInicio = (e.target.fecha.value).replace(" ","").split("-")[0];
+            const fechaFin = (e.target.fecha.value).replace(" ","").split("-")[1];
+            filtros.fechaInicio = fechaInicio +" 00:00:00";
+            filtros.fechaFin = fechaFin +" 23:59:59";
+        }
+        if((e.target.estado.value).length> 0){
+            filtros.estado = estado
+        }
+
+        setFiltros(filtros);        
+    }
+
     return (
         <Accordion activeIndex={0} className="incidencia-filtros-card">
             <AccordionTab header="Filtros">
-            <div className="incidencias-filtros">
-                    <div className="filtro">
-                        <label htmlFor="id"># Incidencia</label>
-                        <InputText id="id" className="p-inputtext-sm" />
+            <form onSubmit={handleSetFiltros}>
+                <div className="incidencias-filtros">
+                        <div className="filtro">
+                            <label htmlFor="id"># Incidencia</label>
+                            <InputText id="id" className="p-inputtext-sm" name="id"/>
+                        </div>
+                        <div className="filtro">
+                            <label htmlFor="nombres">Nombres</label>
+                            <InputText id="nombres" className="p-inputtext-sm" name="nombres"/>
+                        </div>
+                        <div className="filtro">
+                            <label htmlFor="titulo">Titulo</label>
+                            <InputText id="titulo" className="p-inputtext-sm" name="titulo"/>
+                        </div>
+                        <div className="filtro btn-buscar" >
+                            <Button label="Buscar" type="submit" />
+                        </div>
+                        <div className="filtro">
+                            <label htmlFor="Estado">Fecha Incidencia</label>
+                            <Calendar value={dates} locale="es"  className="p-inputtext-sm" dateFormat="yy/mm/dd" onChange={(e) => setDates(e.value)} selectionMode="range" hideOnRangeSelection name="fecha"/>
+                        </div>
+                        <div className="filtro">
+                            <label htmlFor="Estado">Estado</label>
+                            <Dropdown value={selectedEstado} onChange={(e) => setSelectedEstado(e.value)} options={estados} optionLabel="name" 
+                            placeholder="Seleccione Estado" className="p-inputtext-sm" name="estado"/>
+                        </div>
+                        <div></div>
+                        <div></div>
                     </div>
-                    <div className="filtro">
-                        <label htmlFor="nombres">Nombres</label>
-                        <InputText id="nombres" className="p-inputtext-sm" />
-                    </div>
-                    <div className="filtro">
-                        <label htmlFor="titulo">Titulo</label>
-                        <InputText id="titulo" className="p-inputtext-sm" />
-                    </div>
-                    <div className="filtro btn-buscar" >
-                        <Button label="Buscar" />
-                    </div>
-                    <div className="filtro">
-                        <label htmlFor="Estado">Fecha Incidencia</label>
-                        <Calendar value={dates} locale="es" onChange={(e) => setDates(e.value)} selectionMode="range" readOnlyInput hideOnRangeSelection/>
-                    </div>
-                    <div className="filtro">
-                        <label htmlFor="Estado">Estado</label>
-                        <Dropdown value={selectedEstado} onChange={(e) => setSelectedEstado(e.value)} options={estados} optionLabel="name" 
-                        placeholder="Seleccione Estado" className="p-inputtext-sm" />
-                    </div>
-                    <div></div>
-                    <div></div>
-                </div>
+                </form>
             </AccordionTab>
         </Accordion>        
     )
--- proyecto-final/src/domain/user/pages/Incidencia.jsx
+++ proyecto-final/src/domain/user/pages/Incidencia.jsx
@@ -8,19 +8,43 @@ import IncidenciaFiltros from "../components/IncidenciaFiltros";
 
 export default function Incidencia() {    
     const { aIncidencia, isIncidenciaLoading } = useIncidencia();
-    const [ incidencia, setIncidencia] = useState([]);    
+    const [ incidencia, setIncidencia] = useState([]);
+    const [ incidenciaFiltros, setIncidenciaFiltros] = useState([]);
+    const [ filtros, setFiltros ] = useState({});
 
-    useEffect(() => {
+    const cargarData= ()=>{
         if (!isIncidenciaLoading) {
             setIncidencia(aIncidencia);
+            setIncidenciaFiltros(aIncidencia);
         }
-    }, [aIncidencia, isIncidenciaLoading]);
-
-
-    if (isIncidenciaLoading) {
-        return <div>Cargando ...</div>;
+        if(filtros.id || filtros.nombres || filtros.titulo || filtros.fechaInicio || filtros.fechaFin || filtros.estado){
+            console.log(filtros);
+            filtrarIncidencias(filtros);
+        }
     }
-    
+
+    useEffect(() => {
+        cargarData();
+    }, [incidencia, isIncidenciaLoading,filtros]);
+
+    const filtrarIncidencias = (filtros)=>{
+
+        setIncidenciaFiltros(incidencia.filter(element => {
+
+            const fechaIncidencia = new Date(element.fecha_incidencia);
+            const fechaInicio = filtros.fechaInicio ? new Date(filtros.fechaInicio) : null;
+            const fechaFin = filtros.fechaFin ? new Date(filtros.fechaFin) : null;
+            
+            return (
+                (filtros.id && element.id === filtros.id) ||
+                (filtros.nombres && element.nombres.includes(filtros.nombres)) ||
+                (filtros.titulo && element.titulo.includes(filtros.titulo)) ||
+                (filtros.estado && element.estado === filtros.estado) || 
+                (fechaInicio && fechaFin && fechaIncidencia >= fechaInicio && fechaIncidencia <= fechaFin)
+            );
+        }));
+    }
+    /* {I} - Funciones complementarias*/ 
     const statusBodyTemplate = (incidencia) => {
         return <Tag value={incidencia.estado} severity={getSeverity(incidencia)}></Tag>;
     };
@@ -40,13 +64,18 @@ export default function Incidencia() {
                 return null;
         }
     };
+    /* {F} - Funciones complementarias*/
+
+    if (isIncidenciaLoading) {
+        return <div>Cargando ...</div>;
+    }
 
     return (
         <Layout>
         <div className='listado-incidencias'>
             <h1>Listado de Incidencias</h1>
-            <IncidenciaFiltros/>
-            <DataTable value={incidencia} tableStyle={{ minWidth: '50rem' }} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]}>
+            <IncidenciaFiltros setFiltros={setFiltros}/>
+            <DataTable value={incidenciaFiltros} tableStyle={{ minWidth: '50rem' }} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]}>
                 <Column field="id" header="# Incidencia" style={{ width: '20%' }}></Column>
                 <Column field="nombres" header="Nombres" style={{ width: '20%' }}></Column>
                 <Column field="titulo" header="Titulo" style={{ width: '20%' }}></Column>
